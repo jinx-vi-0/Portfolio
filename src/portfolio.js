c6bca88eb728d4ed346df9fb4a18ead03c389f6e
jinx-vi-0 <120160355+jinx-vi-0@users.noreply.github.com>
2024-03-27 13:07:12 +0530

    updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -831,13 +831,13 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "Feel free to reach out to me. Whether it's a question, a project you need guidance on web development, open source projects, competitive programming, data structure and algorithms, or just a friendly chat about tech, I'm all ears! Looking forward to hearing from you soon.",
   },
   blogSection: {
     title: "Blogs",
     subtitle:
-      "For individual fundamental empowerment, I like to write powerful lessons that create impact on each of the reader individually to change the core of their character.",
-    link: "https://blogs.ashutoshhathidara.com/",
+      "I write tech blogs aiming to empower readers with impactful lessons, fostering fundamental changes in their understanding, ultimately enhancing their individual growth.",
+    link: "https://codeforces.com/blog/jinx_vi",
     avatar_image_path: "blogs_image.svg",
   },
   addressSection: {
